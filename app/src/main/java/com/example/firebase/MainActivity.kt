d4babf433ba26f7adadc947137bce803a1341443
AbduxalilVoxidjonov <abduhalilvohidjonov@gmail.com>
2024-02-08 10:49:05 +0500

    Second

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import com.example.firebase.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.CoroutineScope
@@ -25,22 +26,84 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // save data to firestore
         binding.saveBtn.setOnClickListener {
-            val person = Person(
-                binding.firstName.text.toString(),
-                binding.lastName.text.toString(),
-                binding.age.text.toString().toInt()
-            )
+            val person = getOldPersons()
             savePerson(person)
-            binding.firstName.text.clear()
-            binding.lastName.text.clear()
-            binding.age.text.clear()
         }
 //        subscribeToRealtimeUpdates()
 
         binding.retrevieBtn.setOnClickListener {
             retrievePersons()
         }
+        binding.btnUploadData.setOnClickListener {
+            val person = getOldPersons()
+            val newMapPerson = getNewPersonMap()
+            updatePerson(person, newMapPerson)
+        }
+
+
     }
+
+    private fun getOldPersons(): Person {
+        val firstName = binding.firstName.text.toString()
+        val lastName = binding.lastName.text.toString()
+        val age = binding.age.text.toString().toInt()
+        return Person(firstName = firstName, lastName = lastName, age = age)
+    }
+
+    private fun getNewPersonMap(): Map<String, Any> {
+        val firstName = binding.newfirstName.text.toString()
+        val lastName = binding.newlastName.text.toString()
+        val age = binding.newage.text.toString()
+        val map = mutableMapOf<String, Any>()
+        if (firstName.isNotEmpty()) {
+            map["firstName"] = firstName
+        }
+        if (lastName.isNotEmpty()) {
+            map["lastName"] = lastName
+        }
+        if (age.isNotEmpty()) {
+            map["age"] = age
+        }
+        return map
+
+    }
+
+    private fun updatePerson(person: Person, newMapPerson: Map<String, Any>) =
+        CoroutineScope(Dispatchers.IO).launch {
+            val personQuery = personCollectionRef
+                .whereEqualTo("firstName", person.firstName)
+                .whereEqualTo("lastName", person.lastName)
+                .whereEqualTo("age", person.age)
+                .get()
+                .await()
+            if (personQuery.documents.isNotEmpty()) {
+                for (document in personQuery) {
+                    try {
+                        personCollectionRef.document(document.id)
+                            .update("firstName", person.firstName).await()
+                        personCollectionRef.document(document.id).set(
+                            newMapPerson,
+                            SetOptions.merge()
+                        ).await()
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, "Updated", Toast.LENGTH_LONG).show()
+                        }
+                    } catch (e: Exception) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "No person matched the query",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
 
     // Realtime updates
     private fun subscribeToRealtimeUpdates() {
