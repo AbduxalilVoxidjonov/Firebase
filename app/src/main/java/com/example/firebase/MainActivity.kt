d1798756c1dfe4c9ea21b697e92c5854ceb31357
AbduxalilVoxidjonov <abduhalilvohidjonov@gmail.com>
2024-02-08 19:31:04 +0500

    Second

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.widget.Toast
 import com.example.firebase.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.firestore
@@ -40,9 +42,37 @@ class MainActivity : AppCompatActivity() {
             updatePerson(person, newMapPerson)
         }
 
+        binding.btnDeletedata.setOnClickListener {
+            val person = getOldPersons()
+            deletePerson(person)
+        }
+
+        binding.btnBatchesWrite.setOnClickListener {
+            changeName("5LWAMXrDyAsKj7Kg6ogq", "Abu", "Abus")
+        }
 
     }
 
+
+    private fun changeName(
+        personId: String,
+        newFirstName: String,
+        newLastName: String
+    ) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            Firebase.firestore.runBatch { batch ->
+                val personRef = personCollectionRef.document(personId)
+                batch.update(personRef, "firstName", newFirstName)
+                batch.update(personRef, "lastName", newLastName)
+            }.await()
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    // Get data from the user
     private fun getOldPersons(): Person {
         val firstName = binding.firstName.text.toString()
         val lastName = binding.lastName.text.toString()
@@ -50,6 +80,7 @@ class MainActivity : AppCompatActivity() {
         return Person(firstName = firstName, lastName = lastName, age = age)
     }
 
+    // Get data from the user
     private fun getNewPersonMap(): Map<String, Any> {
         val firstName = binding.newfirstName.text.toString()
         val lastName = binding.newlastName.text.toString()
@@ -68,6 +99,44 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // deleted data from firestore
+    private fun deletePerson(person: Person) =
+        CoroutineScope(Dispatchers.IO).launch {
+            val personQuery = personCollectionRef
+                .whereEqualTo("firstName", person.firstName)
+                .whereEqualTo("lastName", person.lastName)
+                .whereEqualTo("age", person.age)
+                .get()
+                .await()
+            if (personQuery.documents.isNotEmpty()) {
+                for (document in personQuery) {
+                    try {
+                        personCollectionRef.document(document.id).delete().await()
+//                        personCollectionRef.document(document.id).update(mapOf("firstName" to FieldValue.delete())).await()
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, "Deleted", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    } catch (e: Exception) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "No person matched the query",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+
+    // Update data in firestore
     private fun updatePerson(person: Person, newMapPerson: Map<String, Any>) =
         CoroutineScope(Dispatchers.IO).launch {
             val personQuery = personCollectionRef
@@ -86,11 +155,13 @@ class MainActivity : AppCompatActivity() {
                             SetOptions.merge()
                         ).await()
                         withContext(Dispatchers.Main) {
-                            Toast.makeText(this@MainActivity, "Updated", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this@MainActivity, "Updated", Toast.LENGTH_LONG)
+                                .show()
                         }
                     } catch (e: Exception) {
                         withContext(Dispatchers.Main) {
-                            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG)
+                                .show()
                         }
                     }
                 }
